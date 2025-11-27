362ae1f2c8e325dcfa273a12462dba835e86f562
a71143055 <a71143055@gmail.com>
2025-11-28 00:03:55 +0900

    mtnmt

--- src/mtnmt/setup.py
+++ src/mtnmt/setup.py
@@ -4,14 +4,19 @@ import pybind11
 ext_modules = [
     Extension(
         "material_bindings",
-        sources=["bindings.cpp", "material_model.c"],
+        sources=[
+            "src/mtnmt/bindings.cpp",
+            "src/mtnmt/material_model.c"
+        ],
         include_dirs=[pybind11.get_include()],
         language="c++",
+        extra_compile_args=["/std:c++17"],  # Windows MSVC 컴파일러 옵션
     ),
 ]
 
 setup(
     name="material_bindings",
     version="0.1",
+    description="C bindings for material model using pybind11",
     ext_modules=ext_modules,
 )
